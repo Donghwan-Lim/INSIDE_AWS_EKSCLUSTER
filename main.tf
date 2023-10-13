f9ae40b49318ed81b2317cb5583b08ecea92c538
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-10-13 10:27:20 +0900

    helm config path modify

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ provider "kubernetes" {
 provider "helm" {
   # Configuration options
   kubernetes {
-    config_path = "~/.kube/config"
+    config_path = module.eks-kubeconfig.kubeconfig
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
