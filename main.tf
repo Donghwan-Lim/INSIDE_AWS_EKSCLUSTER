1b820e94f5243ab77caee5f72854ac9d9ace1ecf
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-10-13 10:13:49 +0900

    kubernetes setting add in helm provider

--- main.tf
+++ main.tf
@@ -36,14 +36,19 @@ provider "helm" {
   # Configuration options
   kubernetes {
     config_path = "~/.kube/config"
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
+/* 
  provider "kubectl" {
   kubernetes {
     config_path = "~/.kube/config"
   }
 }
+*/
 
 locals {
   common-tags = {
