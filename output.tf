c58a179f4255862449a4dd4f95397bb3b27d439c
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-12-01 15:38:12 +0900

    output added cluster primary security group id

--- output.tf
+++ output.tf
@@ -4,3 +4,6 @@ output "node-sg-id" {
 output "node-group-sg-id" {
     value = module.eks.eks_managed_node_groups.NODE_GROUP01.security_group_id
 }
+output "eks-cluster-sg-id" {
+    value = module.eks.cluster_primary_security_group_id
+}
